89ccae6120b8d539efff81dc9d7df44c1e6b4470
fovlin <fovlin@163.com>
2026-08-08 08:17:15 +0800

    fmt

--- main.go
+++ main.go
@@ -9,10 +9,18 @@ import (
 func main() {
 
 	var codeTableFile string = "code.table"
-	srcData, err := os.ReadFile(codeTableFile); if err != nil { fmt.Println("[ERROR]",err); return }
+	srcData, err := os.ReadFile(codeTableFile)
+	if err != nil {
+		fmt.Println("[ERROR]", err)
+		return
+	}
 
 	var codeTable map[string]string
-	err_2 := json.Unmarshal(srcData, &codeTable); if err_2 != nil { fmt.Println("[ERROR]",err_2); return }
+	err_2 := json.Unmarshal(srcData, &codeTable)
+	if err_2 != nil {
+		fmt.Println("[ERROR]", err_2)
+		return
+	}
 
 	switch os.Args[1] {
 
@@ -22,25 +30,28 @@ func main() {
 	case "decrypt":
 		decrypt(codeTable)
 
-	default :
-		fmt.Printf("Unknow command \"%s\"\n",os.Args[1])
+	default:
+		fmt.Printf("Unknow command \"%s\"\n", os.Args[1])
 	}
-
 
 }
 
 func encrypt(codeTable map[string]string) {
 
 	var filename string = os.Args[2]
-	text, err := os.ReadFile(filename); if err != nil { fmt.Println("[ERROR]",err); return }
+	text, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("[ERROR]", err)
+		return
+	}
 
 	var codeList []string
-	for _, index := range(text) {
+	for _, index := range text {
 
 		code := codeTable[string(index)]
 
 		if code == "" {
-			fmt.Printf("Can't found char code of %q.\n",index)
+			fmt.Printf("Can't found char code of %q.\n", index)
 			return
 		}
 
@@ -48,45 +59,56 @@ func encrypt(codeTable map[string]string) {
 
 	}
 
-	data, err := json.Marshal(codeList); if err != nil { fmt.Println("[ERROR]",err); return }
-	os.WriteFile(filename + ".enc", data, 0666)
-
+	data, err := json.Marshal(codeList)
+	if err != nil {
+		fmt.Println("[ERROR]", err)
+		return
+	}
+	os.WriteFile(filename+".enc", data, 0666)
 
 }
 
 func decrypt(codeTable map[string]string) {
 
 	var filename string = os.Args[2]
-	code, err := os.ReadFile(filename); if err != nil { fmt.Println("[ERROR]",err); return }
+	code, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("[ERROR]", err)
+		return
+	}
 
 	var codeList []string
-	err_2 := json.Unmarshal(code, &codeList); if err_2 != nil { fmt.Println("[ERROR]",err_2); return }
+	err_2 := json.Unmarshal(code, &codeList)
+	if err_2 != nil {
+		fmt.Println("[ERROR]", err_2)
+		return
+	}
 
 	decodeTable := make(map[string]string)
-	for k, v := range(codeTable) {
-		decodeTable[v] = k
+	for key, value := range codeTable {
+		decodeTable[value] = key
 	}
-	
+
 	var charList []string
-	for _, index := range(codeList) {
-		
+	for _, index := range codeList {
+
 		char := decodeTable[index]
 
 		if char == "" {
-			fmt.Printf("Can't found char code of \"%q\".\n",index)
+			fmt.Printf("Can't found char code of \"%q\".\n", index)
 			return
 		}
 
-		charList = append(charList,char)
+		charList = append(charList, char)
 
 	}
 
 	text := ""
 
-	for _, index := range(charList) {
+	for _, index := range charList {
 		text += index
 	}
 
-	os.WriteFile(filename + ".txt", []byte(text),0666)
+	os.WriteFile(filename+".txt", []byte(text), 0666)
 
 }
